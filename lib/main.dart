dc430e7c091f054d5fd3161d03da7a9b71880954
caio <ccaioabreudesouza9874@gmail.com>
2024-07-13 19:08:53 -0300

    Adicionado pop de adicionar poderes

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,19 @@ void main() {
 
 }
 
+enum EfeitoSelect {
+  blue('Blue', Colors.blue),
+  pink('Pink', Colors.pink),
+  green('Green', Colors.green),
+  yellow('Orange', Colors.orange),
+  grey('Grey', Colors.grey);
+
+  const EfeitoSelect(this.label, this.color);
+  final String label;
+  final Color color;
+}
+
+
 class Poderes extends StatefulWidget {
   const Poderes({super.key});
 
@@ -17,27 +30,52 @@ class Poderes extends StatefulWidget {
 }
 
 class _PoderesState extends State<Poderes> {
+  // Declaração de Variáveis
   List _poderes = [];
+  List _efeitos = [];
 
+  // Controles de input 
+  TextEditingController inputTextPoder = TextEditingController();
+  String EfeitoSelecionado = '';
   @override
+  
+  final TextEditingController colorController = TextEditingController();
+  EfeitoSelect? selectedColor;
+
   void initState() {
     super.initState();
     _carregarDados(); // Carrega os dados ao iniciar o estado
   }
   Future<void> _carregarDados() async {
-    // Carrega o arquivo JSON
-    String jsonString = await rootBundle.loadString('assets/poderes.json');
-    List<dynamic> jsonPoderes = jsonDecode(jsonString);
+    // Carrega o Poderes text
+    String jsonPoderes = await rootBundle.loadString('assets/poderes.json');
+    List<dynamic> objPoderes = jsonDecode(jsonPoderes);
 
-    // Converte o JSON em objetos Poder e atualiza o estado
+    // Carrega o Efeitos
+    String jsonEfeitos = await rootBundle.loadString('assets/poderes/efeitos.json');
+    List<dynamic> objEfeitos = jsonDecode(jsonEfeitos);
+
+    // Converte o JSON em objetos
     setState(() {
+      //? Texte Poder
       Map poder = {};
-      for(poder in jsonPoderes){
+      for(poder in objPoderes){
         _poderes.add({
           "nome": poder["nome"],
           "efeito": poder["efeito"],
+          "graduacao": poder["graduacao"],
         });
       }
+
+      Map efeito = {};
+      for(efeito in objEfeitos){
+        _efeitos.add({
+          "e_id": efeito["e_id"],
+          "efeito": efeito["efeito"] 
+        });
+      }
+
+      EfeitoSelecionado = _poderes.first["efeito"];
     });
   }
 
@@ -61,8 +99,10 @@ class _PoderesState extends State<Poderes> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  Text(_poderes[index]['nome']),
-                  Text(_poderes[index]['efeito'])
+                  ListTile(
+                    title: Text(_poderes[index]['nome']),
+                    subtitle: Text("${_poderes[index]['efeito']} ${_poderes[index]['graduacao']}"),
+                  ),                  
                 ],
               ),
             )
@@ -83,25 +123,65 @@ class _PoderesState extends State<Poderes> {
               barrierDismissible: false,
               builder: (BuildContext context) {
                 return AlertDialog(
-                  title: const Text('AlertDialog Title'),
-                  content: const SingleChildScrollView(
+                  title: const Text('Adicionar Poder'),
+                  content: SingleChildScrollView(
                     child: ListBody(
                       children: <Widget>[
-                        Text('This is a demo alert dialog.'),
-                        Text('Would you like to approve of this message?'),
+
+                        //* Entrada do Nome
+                        TextField(
+                          controller: inputTextPoder,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Nome do poder',
+                          ),
+                        ),
+
+                        const SizedBox(height: 15,),
+
+                        DropdownButton<String>(
+                          value: EfeitoSelecionado,
+                          icon: const Icon(Icons.arrow_downward),
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.deepPurple),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.deepPurpleAccent,
+                          ),
+                          onChanged: (String? value) {
+                            // This is called when the user selects an item.
+                            setState(() {
+                              EfeitoSelecionado = value!;
+                            });
+                          },
+                          items: _efeitos.map<DropdownMenuItem<String>>((value) {
+                            return DropdownMenuItem<String>(
+                              value: value["efeito"],
+                              child: Text(value["efeito"].toString()),
+                            );
+                          }).toList(),
+                        )
                       ],
-                    ),
+                    )
                   ),
                   actions: <Widget>[
                     TextButton(
-                      child: const Text('Approve'),
+                      child: const Text('Adicionar'),
+                      onPressed: () {                        
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    TextButton(
+                      child: const Text('Cancelar'),
                       onPressed: () {
                         Navigator.of(context).pop();
                       },
                     ),
                   ],
                 );
+                
               },
+              
             ),
           
           tooltip: 'Increment',
