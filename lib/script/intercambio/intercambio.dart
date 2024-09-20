7b0c4be4c86c240bd20b27b97cd726ae73d88a81
caio <ccaioabreudesouza9874@gmail.com>
2024-09-20 18:46:52 -0300

    adicionado base de bonus intercambio

---
+++ lib/script/intercambio/intercambio.dart
@@ -0,0 +1,12 @@
+class Intercambio {
+  addBonus(String id, List ListBonus){
+    /*
+      Adiciona um bonus a algum pedaço da ficha
+      - Parm:
+        - String id: reflete a identifica quem criou o id é formadado por um
+          TimeStamp em hexastring
+        - List ListBonus: Atributo chave o destino do bonus, e valor é valor do bonus
+
+    */
+  }
+}
